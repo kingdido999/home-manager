5d15905052e12ca083d99812e2e2413409dbce34
Pengcheng Ding <info@pcding.com>
2025-09-06 15:40:55 -0700

    Add wezterm keybindings

--- wezterm.lua
+++ wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 return {
   front_end = "WebGpu",
@@ -7,4 +8,37 @@ return {
   font_size = 14.0,
   default_prog = { os.getenv("HOME") .. "/.nix-profile/bin/fish" },
   audible_bell = "Disabled",
+
+  keys = {
+    {
+      key = '|',
+      mods = 'CTRL|SHIFT|ALT',
+      action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = 'w',
+      mods = 'CMD',
+      action = act.CloseCurrentPane { confirm = true },
+    },
+    {
+      key = 'h',
+      mods = 'CTRL|SHIFT',
+      action = act.ActivatePaneDirection 'Left',
+    },
+    {
+      key = 'l',
+      mods = 'CTRL|SHIFT',
+      action = act.ActivatePaneDirection 'Right',
+    },
+    {
+      key = 'k',
+      mods = 'CTRL|SHIFT',
+      action = act.ActivatePaneDirection 'Up',
+    },
+    {
+      key = 'j',
+      mods = 'CTRL|SHIFT',
+      action = act.ActivatePaneDirection 'Down',
+    },
+  }
 }
